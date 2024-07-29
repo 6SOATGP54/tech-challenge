61227f882176044c1ce27f536390bf70cdad0016
Cledersonbc <cledersonbc@live.com>
2024-07-29 09:11:28 -0300

    added: infra diagram

---
+++ start-cluster.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if kubectl -v &> /dev/null
+then
+	echo "Starting cluster..."
+else
+	echo "Error. Please install Kubectl"
+	echo "visit: https://kubernetes.io/docs/reference/kubectl/kubectl/"
+	exit 1
+fi
+
+# Secrets
+kubectl apply -f infra/yamls/postgres.secret.yaml
+
+# Deployments
+kubectl apply -f infra/yamls/app.deployment.yaml
+
+# Services
+kubectl apply -f infra/yamls/redis-service.yaml
+kubectl apply -f infra/yamls/postgres.service.yaml
+kubectl apply -f infra/yamls/app-food-service.yaml
+
+# AutoScale
+kubectl apply -f infra/yamls/hpa.yaml
